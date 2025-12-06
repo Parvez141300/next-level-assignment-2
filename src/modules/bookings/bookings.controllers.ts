4e17a3f0c7dba1f6b026a96fd1672c3d2c94ca93
Parvez141300 <parvezhossain744471@gmail.com>
2025-12-06 20:12:05 +0600

    created a end point called create bookig

--- src/modules/bookings/bookings.controllers.ts
+++ src/modules/bookings/bookings.controllers.ts
@@ -1,8 +1,32 @@
 import { Request, Response } from "express";
+import { bookingsServices } from "./bookings.services";
 
 // create booking req and res handle
 const createBooking = async (req: Request, res: Response) => {
-
+    try {
+        const result = await bookingsServices.createBookingIntoDB(req.body) as any;
+        console.log(result);
+        if (result.total_price) {
+            res.status(200).json({
+                success: true,
+                message: "Booking created successfully",
+                data: result,
+            })
+        }
+        else{
+            res.status(404).json({
+                success: false,
+                message: "Booking failed",
+                errors: "Resource doesn't exist"
+            })
+        }
+    } catch (error: any) {
+        res.status(403).json({
+            success: false,
+            message: "Valid token but insufficient permissions",
+            errors: error.message
+        })
+    }
 }
 
 export const bookingsControllers = {
--- src/modules/bookings/bookings.services.ts
+++ src/modules/bookings/bookings.services.ts
@@ -3,10 +3,39 @@ import { pool } from "../../config/db"
 const createBookingIntoDB = async (payload: Record<string, unknown>) => {
     const { customer_id, vehicle_id, rent_start_date, rent_end_date } = payload;
 
-    const result = await pool.query(`
-        INSERT INTO Bookings(customer_id, vehicle_id, rent_start_date, rent_end_date)`, [customer_id, vehicle_id, rent_start_date, rent_end_date]);
+    // vehicle related query
+    const vehicleResult = await pool.query(`
+        SELECT * From Vehicles Where id=$1
+        `, [vehicle_id]);
 
-    return result;
+    if (vehicleResult.rowCount === 0) {
+        return null
+    }
+
+    if (vehicleResult.rows[0].availability_status !== "available") {
+        return null
+    }
+
+    // calculate days
+    const startDate = new Date(rent_start_date as string);
+    const endDate = new Date(rent_end_date as string);
+    const number_of_days = Math.ceil(((endDate as any) - (startDate as any)) / (1000 * 60 * 60 * 24));
+    // calculate price
+    const total_price = vehicleResult.rows[0].daily_rent_price * number_of_days;
+
+
+
+    // booking related query
+    const result = await pool.query(`
+        INSERT INTO Bookings(customer_id, vehicle_id, rent_start_date, rent_end_date, total_price, status) VALUES($1, $2, $3, $4, $5, $6) RETURNING *`, [customer_id, vehicle_id, rent_start_date, rent_end_date, total_price, 'active']);
+
+    if(result.rowCount !== 0 && vehicleResult.rows[0].availability_status === "available"){
+        await pool.query(`
+            UPDATE Vehicles SET availability_status=$1 WHERE id=$2
+            `, ['booked', vehicle_id]);
+    }
+    
+    return { ...result.rows[0], vehicle: { vehicle_name: vehicleResult.rows[0].vehicle_name, daily_rent_price: vehicleResult.rows[0].daily_rent_price } };
 }
 
 export const bookingsServices = {
